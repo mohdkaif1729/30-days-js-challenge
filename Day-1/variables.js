70c6aa1f227c16f71a3fb9c2fd7502538f8c74b3
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-07-13 08:48:15 +0530

    Day 1 challenge is completed

---
+++ Day-1/variables.js
@@ -0,0 +1,55 @@
+// Activity 1: Vaiable Declaration
+var scoreOfIndiaInFinal = 176;
+console.log(scoreOfIndiaInFinal);
+let scoredCountryName1 = "India";
+console.log(scoredCountryName1);
+
+// Activity 2: Constant Declaration
+const isIndiaWinsIn2024T_20 = true;
+console.log(isIndiaWinsIn2024T_20);
+
+// Activity 3: Data Types
+const scoreOfSouthAfrica = 171;
+console.log(typeof scoreOfSouthAfrica);
+const scoredCountryName2 = "South Africa";
+console.log(typeof scoredCountryName2);
+const isSouthAfricaWinsIn2024T_20 = false;
+console.log(typeof isSouthAfricaWinsIn2024T_20);
+const matchBetweenWhichTwoCountries = {
+  country1: {
+    name: "India",
+    wins: true,
+    bat: "first",
+  },
+  country2: {
+    name: "South Africa",
+    wins: false,
+    bat: "second",
+  }
+}
+console.log(typeof matchBetweenWhichTwoCountries);
+const totalTeamsPlayInT_20In2024 = [
+  "Afghanistan",
+  "Australia",
+  "Bangladesh",
+  "Canada",
+  "England squad",
+  "India",
+  "West Indies",
+  "United States",
+  "more..."
+];
+console.log(typeof totalTeamsPlayInT_20In2024);
+
+// Activity 4: Reassigning Vaiable
+let scoreInT_20 = 176;
+console.log(scoreInT_20);
+
+scoreInT_20 = 171;
+console.log(scoreInT_20);
+
+// Activity 5: Understanding Const
+const winCountryInT_20 = "india";
+console.log(winCountryInT_20);
+winCountryInT_20 = "south africa";
+console.log(winCountryInT_20);
