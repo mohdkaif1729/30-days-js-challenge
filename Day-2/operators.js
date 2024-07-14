47fcfae54078e2c6c503161d9df21202883a04d6
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-07-14 09:45:49 +0530

    Day 2 challenge is completed

---
+++ Day-2/operators.js
@@ -0,0 +1,34 @@
+// Activity 1: Arithmetic Operator
+const num1 = 5;
+const num2 = 2;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+// Activity 2: Assignment Operator
+let addition = 5;
+let subtraction = 2;
+const num = 3;
+addition += num;
+subtraction += num;
+console.log(addition);
+console.log(subtraction);
+
+// Activity 3: Comparison Operator
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+// Activity 4: Logical Operator
+console.log(num1 && num2);
+console.log(num1 || num2);
+console.log(!num2);
+
+// Activity 5: Ternary Operator
+const result = num > 0 ? "positive" : "negative";
+console.log(result);
